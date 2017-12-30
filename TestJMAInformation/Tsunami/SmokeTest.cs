9ad8db0d1b62268c6efc3ffa7179f7a47f1816c7
typewriter <takuyaxp@gmail.com>
2017-12-30 15:23:44 +0900

    Add tsunami data parser(converter)

---
+++ TestJMAInformation/Tsunami/SmokeTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using P2PQuake.JMAInformation.Tsunami;
+using P2PQuake.JMAInformation.Tsunami.Convert;
+
+namespace TestJMAInformation.Tsunami
+{
+    [TestFixture]
+    class SmokeTest
+    {
+        private static IEnumerable<TestCaseData> GenerateTestCases()
+        {
+            string directory = @"../../TestData/Tsunami";
+            string[] files = Directory.GetFiles(directory, "*.htm*");
+
+            foreach (string path in files)
+            {
+                string expectedFileName = Path.GetFileNameWithoutExtension(path) + ".json";
+                string expectedPath = Path.Combine(directory, expectedFileName);
+                yield return new TestCaseData(path, expectedPath);
+            }
+        }
+
+        [Test, TestCaseSource("GenerateTestCases")]
+        public void JMATsunamiConverterTest(string sourcePath, string expectedPath)
+        {
+            StreamReader reader = new StreamReader(sourcePath, Encoding.UTF8);
+            string content = reader.ReadToEnd();
+            reader.Close();
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("--" + Path.GetFileName(sourcePath));
+
+            Core core = JMATsunamiConverter.fromString(content);
+            string json = JsonConvert.SerializeObject(core, Formatting.Indented);
+
+            Console.WriteLine("--");
+            Console.WriteLine(json);
+
+            if (!File.Exists(expectedPath))
+            {
+                StreamWriter writer = new StreamWriter(expectedPath, false, Encoding.UTF8);
+                writer.Write(json);
+                writer.Close();
+
+                Assert.Ignore();
+                return;
+            }
+
+            StreamReader expectedReader = new StreamReader(expectedPath, Encoding.UTF8);
+            string expectedJson = expectedReader.ReadToEnd();
+            expectedReader.Close();
+
+            Assert.AreEqual(expectedJson, json);
+        }
+    }
+}
